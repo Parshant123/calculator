ac5d1e0a21b39280dc3f23faa75ec703f3335347
Parshant Dhariwal <dhariwalparshant@gmail.com>
2022-04-02 21:39:19 +0530

    function acendingOrder

---
+++ callbackfunction.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function ascendingOrder(arr) {
+    let counter =0;
+    for (let counter = 0; counter < arr.length; counter = counter + 1) {
+        if ( arr[counter>0]);
+        
+            return (arr[counter]);
+    } 
+   
+}
+let acending = ascendingOrder[1, 8, 5, 7, 9];
+console.log(acending);
